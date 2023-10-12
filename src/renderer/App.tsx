aa1051fa64dd2f22d8cf212988638bb8f92c6d99
techguru0 <130264564+techguru0@users.noreply.github.com>
2023-10-12 11:42:49 -0400

    Dependency cycle fixed

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import { useState, createContext } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 import { url } from 'inspector';
